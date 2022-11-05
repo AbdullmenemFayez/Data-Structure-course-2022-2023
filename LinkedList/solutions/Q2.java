2f16c5ea6c63eba0808da71cd88de4dafef46b6d
Nada Jaradat <86928581+nadajaradat@users.noreply.github.com>
2022-11-05 21:37:20 +0200

    Create Q2.java

---
+++ LinkedList/solutions/Q2.java
@@ -0,0 +1,39 @@
+public static void main(String[] args) {
+        MyLinkedList l = new MyLinkedList<>();
+        l.addLast(1);
+        l.addLast(2);
+        l.addLast(3);
+        l.addLast(4);
+        System.out.println(l.toString());
+        MyLinkedList ans = new MyLinkedList<>();;
+        ans.head = rotateLeft(l.head, 1);
+        System.out.println(ans.toString());
+
+    }
+
+public static Node rotateLeft(Node head, int k) {
+        //to find tail and length of linked list
+        int len = 1;
+        Node tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            len++;
+        }
+
+        k = k % len;
+        if (k == 0) {
+            return head;
+        }
+
+        Node curr = head;
+        while (--k > 0) {
+            curr = curr.next;
+        }
+
+        Node newHead = curr.next;
+        tail.next = head;
+        curr.next = null;
+
+        return newHead;
+
+    }
