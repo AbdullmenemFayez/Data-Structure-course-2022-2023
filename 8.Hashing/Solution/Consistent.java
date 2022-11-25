95b3b1670f8d6e8e7bd4924f5c779ec2c06f5df9
Alaa.N.Duridi <51034664+AlaaDuridi@users.noreply.github.com>
2022-11-25 19:53:29 +0200

    Create Consistent.java

---
+++ 8.Hashing/Solution/Consistent.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+/**
+ *
+ * @author AlaaDuridi
+ */
+public class Consistent {
+
+    public static void main(String[] args) {
+
+        String allowed = "andelr";
+        String[] words = {"alaa", "nader", "noor", "abrar", "ahmad"};
+
+        Map<Character, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < allowed.length(); ++i) {
+
+            hm.put(allowed.charAt(i), hm.getOrDefault(allowed.charAt(i), 0) + 1);
+        }
+
+        int sum = 0;
+        for (int i = 0; i < words.length; ++i) {
+            boolean flag = true;
+
+            for (int j = 0; j < words[i].length(); ++j) {
+
+                if (hm.getOrDefault(words[i].charAt(j), 0) <= 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                sum++;
+            }
+
+        }
+
+        System.out.println(sum);
+    }
+
+}
