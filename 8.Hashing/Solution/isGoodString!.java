11cab501bcd3027552401205cb0e2b58858d6b31
Alaa.N.Duridi <51034664+AlaaDuridi@users.noreply.github.com>
2022-12-02 13:58:17 +0200

    Update isGoodString!.java

--- 8.Hashing/Solution/isGoodString!.java
+++ 8.Hashing/Solution/isGoodString!.java
@@ -1,38 +1,22 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-/**
- *
- * @author AlaaDuridi
- */
-public class goodString {
-
-    public static void main(String[] args) {
-        // driver code to test your method 
-        String s = "aaabbb";
-        System.out.println(isGood(s));
-
-    }
 
     private static boolean isGood(String s) {
-        if (s.equals("")) {
+        if (s.equals("")) 
             return true;
-        }
+        
 
         HashMap<Character, Integer> hm = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) 
 
             hm.put(s.charAt(i), hm.getOrDefault(s.charAt(i), 0) + 1);
-        }
+        
 
         Set<Integer> set = new HashSet<>();
 
-        for (Integer i : hm.values()) {
+        for (Integer i : hm.values()) 
             set.add(i);
-        }
+        
         return set.size() == 1;
 
     }
 
-}
+
